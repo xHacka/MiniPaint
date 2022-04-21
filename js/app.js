91d96becb82b71076c21c7b66663e768f9ce51fa
xHacka <pvpgamerpro457@gmail.com>
2022-04-21 19:05:26 +0300

    Fixed Miron Styling

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ import { useRedo, useUndo } from "./timeline.js";
 
 // Variables
 const tools = Array.from(document.querySelectorAll(".tools-item"));
+const toolsIcons = Array.from(document.querySelectorAll('.tools-item i'))
 const slider = document.querySelector(".size-slider");
 const bucket = document.querySelector(".tools-item i[data-hover=Bucket]");
 const clear = document.querySelector(".tools-item i[data-hover=Clear]");
@@ -14,12 +15,15 @@ const save = document.querySelector(".save");
 
 // Event Handlers
 
-for (const tool of tools) {
+for (const [i, tool] of tools.entries()) {
   tool.addEventListener("click", () => {
+
     tool.classList.add("tools-item--active");
-    tools
-      .filter((i) => i !== tool)
-      .map((r) => r.classList.remove("tools-item--active"));
+    toolsIcons[i].classList.add("tools-item--active-color")
+    
+    tools.filter((i) => i !== tool).map((r) => r.classList.remove("tools-item--active"));
+    toolsIcons.filter((icon) => icon !== toolsIcons[i]).map((r) => r.classList.remove("tools-item--active-color")); 
+
   });
 }
 
@@ -27,10 +31,7 @@ slider.addEventListener("input", () => mt.changeSize());
 
 mt.canvas.addEventListener("mousedown", mt.drawStart);
 mt.canvas.addEventListener("mouseup", mt.drawFinish);
-mt.canvas.addEventListener("mousemove", (e) => {
-  mt.draw(e);
-  mt.displayToolBorder(e);
-});
+mt.canvas.addEventListener("mousemove", (e) => { mt.draw(e); mt.displayToolBorder(e); });
 
 bucket.addEventListener("click", mt.useBucket);
 clear.addEventListener("click", mt.useClear);
